1a108de561b28268ab8cd9427956aef9eb1c473e
OiseauPython <stankojeremy96@gmail.com>
2018-11-08 22:17:58 +0100

    début

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'My blog made with Angular !';
+    pageTitle = 'My blog made with Angular !';
+    
+    post = [{title : 'First article', content : 'A very cool article', loveIt: 2, createAt:Date}, 
+            {title : 'Second article', content : 'A seconde very cool article', loveIt: -2, createAt:Date },
+            {title : 'Another one', content : 'Awesome article', loveIt: 1, createAt:Date}];
+
 }
